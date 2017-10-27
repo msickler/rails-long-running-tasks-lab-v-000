899fcb0dc6823d53c147061ca1489146a931177d
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 21:09:02 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,16 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
+  end
+
+  def upload
+    CSV.foreach(params[:file].path, headers: true) do |song|
+      @artist = Artist.find_or_create_by(name: song[1])
+      Song.create(title: song[0], artist_name: @artist.name)
+    end
+    redirect_to songs_path
   end
 
   def show
@@ -51,4 +60,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
